ea60586a604b7f91f4d9b0cdd67671028b5571a7
Samuel Huang <samhuang91@gmail.com>
2020-12-29 10:36:43 -0800

    Implement PopulationChart component

---
+++ app/components/charts/population-chart.js
@@ -0,0 +1,54 @@
+import BaseChart from './base-chart';
+
+// https://www.highcharts.com/demo/line-basic
+export default class PopulationChart extends BaseChart {
+  chartOptions = {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Historic World Population by Region'
+    },
+    subtitle: {
+      text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
+    },
+    xAxis: {
+      categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+      title: {
+        text: null
+      }
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Population (millions)',
+        align: 'high'
+      },
+      labels: {
+        overflow: 'justify'
+      }
+    },
+    tooltip: {
+      valueSuffix: ' millions'
+    },
+    plotOptions: {
+      bar: {
+        dataLabels: {
+          enabled: true
+        }
+      }
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'top',
+      x: -40,
+      y: 80,
+      floating: true,
+      borderWidth: 1,
+      backgroundColor:
+        Highcharts.defaultOptions.legend.backgroundColor || '#FFFFFF',
+      shadow: true
+    }
+  };
+}
