d90cf1b96bb2e3d073ee6d21348f206fba3e688b
Samuel Huang <samhuang91@gmail.com>
2020-12-29 10:37:06 -0800

    Implement HeightWeightChart component

---
+++ app/components/charts/height-weight-chart.js
@@ -0,0 +1,65 @@
+import BaseChart from './base-chart';
+
+// https://www.highcharts.com/demo/scatter
+export default class HeightWeightChart extends BaseChart {
+  chartOptions = {
+    chart: {
+      type: 'scatter',
+      zoomType: 'xy'
+    },
+    title: {
+      text: 'Height Versus Weight of 507 Individuals by Gender'
+    },
+    subtitle: {
+      text: 'Source: Heinz  2003'
+    },
+    xAxis: {
+      title: {
+        enabled: true,
+        text: 'Height (cm)'
+      },
+      startOnTick: true,
+      endOnTick: true,
+      showLastLabel: true
+    },
+    yAxis: {
+      title: {
+        text: 'Weight (kg)'
+      }
+    },
+    legend: {
+      layout: 'vertical',
+      align: 'left',
+      verticalAlign: 'top',
+      x: 100,
+      y: 70,
+      floating: true,
+      backgroundColor: Highcharts.defaultOptions.chart.backgroundColor,
+      borderWidth: 1
+    },
+    plotOptions: {
+      scatter: {
+        marker: {
+          radius: 5,
+          states: {
+            hover: {
+              enabled: true,
+              lineColor: 'rgb(100,100,100)'
+            }
+          }
+        },
+        states: {
+          hover: {
+            marker: {
+              enabled: false
+            }
+          }
+        },
+        tooltip: {
+          headerFormat: '<b>{series.name}</b><br>',
+          pointFormat: '{point.x} cm, {point.y} kg'
+        }
+      }
+    }
+  };
+}
