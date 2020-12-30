74f46e529ec1e071b8c302fec3b5f654218d489e
Samuel Huang <samhuang91@gmail.com>
2020-12-30 11:39:56 -0800

    Rename ChartDemo to AllCharts

--- tests/integration/components/all-charts-test.js
+++ tests/integration/components/all-charts-test.js
@@ -3,14 +3,14 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | chart-demo', function(hooks) {
+module('Integration | Component | all-charts', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<ChartDemo />`);
+    await render(hbs`<AllCharts />`);
 
     const charts = this.element.getElementsByClassName('chart');
 
